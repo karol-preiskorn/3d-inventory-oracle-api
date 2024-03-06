af92fafcae2507bbe324bb7ffdc26555c177573a
C2RLO <karol-preiskorn@users.noreply.github.com>
2024-03-06 20:16:09 +0100

    Add initial database tables and design models
    The commit adds the initial database tables for attribute types, attributes, connections, devices, and logs. Additionally, it includes the creation of business information, change request, and data types design models. These changes lay the foundation for data management and structuring in the system.
    
    Resolves: #123

---
+++ src-cli.sh
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/bash
+#
+# File:        /src-cli.sh
+# Description:
+# Used by:
+# Dependency:
+#
+# Date         By        Comments
+# ----------   -------   ------------------------------
+# 2023-03-28   C2RLO
+#
+
+rm -rf lib-swagger-client -p tmp
+SPECFILE=api/openapi.yaml
+wget --quiet --no-check-certificate $SPECURL -O $SPECFILEdocker run --rm --net=host -u="$(id -u)" -v ${PWD}:/local swaggerapi/swagger-codegen-cli:3.0.41 generate \
+  -i /local/$SPECFILE \
+  -l typescript-angular \
+  -o lib-swagger-client/src \
+  --additional-properties ngVersion=15.2.2
